497c3d5104092fe99d4513cda55f2d2ddfe43bfa
malinda <malisur490@gmail.com>
2024-06-22 22:22:05 +0530

    testing

--- web/routes/admin.js
+++ web/routes/admin.js
@@ -1,18 +1,21 @@
 const route = require("express").Router();
-const {AdminLogin,AddAdmin,GetAccountInfo,ChangeUserName,ChangeContact,CheckPrePassword,ChangePassword} = require("../controllers/admin.js");
-const auth = require("../middleware/auth.js")
-
-
+const {
+  AdminLogin,
+  AddAdmin,
+  GetAccountInfo,
+  ChangeUserName,
+  ChangeContact,
+  CheckPrePassword,
+  ChangePassword,
+} = require("../controllers/admin.js");
+const auth = require("../middleware/auth.js");
 
 route.post("/", AdminLogin);
 route.post("/addAdmin", AddAdmin);
-route.get("/getinfo",GetAccountInfo);
+route.get("/getinfo", GetAccountInfo);
 route.post("/changeUsername", ChangeUserName);
 route.post("/changeContact", ChangeContact);
 route.post("/CheckPrePassword", CheckPrePassword);
 route.post("/ChangePassword", ChangePassword);
-
-
-
 
 module.exports = route;
