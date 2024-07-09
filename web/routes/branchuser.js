6aa27f69439614210ffc6b8181ec4b8a27f605e1
Malinda Suresh <142488296+Madhushankms@users.noreply.github.com>
2024-07-09 14:29:21 +0530

    Delete web directory

--- web/routes/branchuser.js
+++
@@ -1,7 +0,0 @@
-const webBranchuser = require ("express").Router();
-const {getRegisterdpersonDetailsList,getRegisterdpersonDetailsById}=require("../controllers/branchuser.js")
-
-webBranchuser.get("/RegisterdpersonDetails",getRegisterdpersonDetailsList);
-webBranchuser.get("/RegisterdpersonDetailsbyid/:id",getRegisterdpersonDetailsById);
-
-module.exports = webBranchuser; 
