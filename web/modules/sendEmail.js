d84e7e32a48fa92f04500efdc375cd1000f20046
dilsara <patabadigerajitha@gmail.com>
2024-07-10 12:46:06 +0530

    Mail style

--- web/modules/sendEmail.js
+++ web/modules/sendEmail.js
@@ -48,10 +48,45 @@ module.exports = {
       from: "ssshashini21@gmial.com",
       to: values.Email,
       subject: `Congratulations ${values.FirstName}! You have been choosen up.`,
-      text: `Your ID: ${values.admin_Id}
-            Name: ${values.FirstName}
-            Your Password: ${values.originalPassword}
-            Use your email as your user name!`,
+      // text: `Your ID: ${values.admin_Id}
+      //       Name: ${values.FirstName}
+      //       Your Password: ${values.originalPassword}
+      //       Use your email as your user name!`,
+
+      html: `
+    <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #FFFFFF;
+            padding: 20px;
+          }
+          .header {
+            background-color: #4caf50;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            border-radius: 10px 10px 0 0;
+          }
+          .content {
+            padding: 20px;
+          }
+        </style>
+      </head>
+      <body>
+          <div class="header">
+            <h2>Your Order Confirmation</h2>
+          </div>
+          <div class="content">
+            <h2>Welcome to Xpress !</h2>
+            <p>Hi ${values.Email} user,</p>
+            <p>Your order ${values.Order_id} has been successfully assigned to ${values.branchLocation} branch.</p>
+            <p>Your package's timely arrival is our priority.</p>
+          </div>
+      </body>
+    </html>
+  `,
     };
 
     transporter.sendMail(adminMailOptions,(error, info) => {
